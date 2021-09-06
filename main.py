b0007788efb64e5c0bd49caca400eab61f9cb998
Yakub5621 <>
2021-09-06 17:24:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band generator ")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print(city + " " + pet + " is your band name")
+
